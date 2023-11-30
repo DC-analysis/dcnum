bab2e4b7fbd89cdaf56d28c4d97815e0714b3c34
Paul Müller <dev@craban.de>
2023-11-30 23:34:51 +0100

    feat: implement copy background class

--- src/dcnum/feat/feat_background/__init__.py
+++ src/dcnum/feat/feat_background/__init__.py
@@ -2,5 +2,6 @@
 """Feature computation: background image data from image data"""
 from .base import Background, get_available_background_methods
 # Background methods are registered by importing them here.
+from .bg_copy import BackgroundCopy
 from .bg_roll_median import BackgroundRollMed
 from .bg_sparse_median import BackgroundSparseMed
