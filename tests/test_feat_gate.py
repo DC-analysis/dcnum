a69e8c5ee18e8a509cc49d2cbbf4063e28fa212c
Paul Müller <dev@craban.de>
2023-12-27 15:51:35 +0100

    tests: increase coverage

--- tests/test_feat_gate.py
+++ tests/test_feat_gate.py
@@ -1,6 +1,7 @@
 from dcnum.feat import Gate
 from dcnum.read import HDF5Data
 import h5py
+import numpy as np
 
 import pytest
 
@@ -26,6 +27,62 @@ def test_features():
         assert "deform" in gt.features
         assert gt.features.count("deform") == 1
         assert gt.features.count("area_um") == 1
+
+
+def test_gate_feature():
+    path = retrieve_data(
+        "fmt-hdf5_cytoshot_full-features_legacy_allev_2023.zip")
+    # Since these are CytoShot data, there are no online filters here.
+    with h5py.File(path, "a") as h5:
+        h5.attrs["online_contour:bin area min"] = 20
+        h5.attrs["online_filter:deform min"] = 0.1
+        h5.attrs["online_filter:deform max"] = 0.5
+        h5.attrs["online_filter:deform soft limit"] = False
+        h5.attrs["online_filter:area_um min"] = 50
+        h5.attrs["online_filter:area_um soft limit"] = False
+
+    with HDF5Data(path) as hd:
+        # sanity checks for later maybe
+        assert np.allclose(
+            hd["deform"][0],
+            0.0740563677588885,
+            atol=0, rtol=1e-5)
+        assert np.allclose(
+            hd["deform"][1],
+            0.4113239579639607,
+            atol=0, rtol=1e-5)
+        assert np.allclose(
+            hd["area_um"][0],
+            0.559682,
+            atol=0, rtol=1e-5)
+        assert np.allclose(
+            hd["area_um"][1],
+            91.193185875,
+            atol=0, rtol=1e-5)
+
+        gt = Gate(data=hd, online_gates=True)
+        assert gt.gate_feature(feat="deform", data=0.3)
+        assert not gt.gate_feature(feat="deform", data=0.51)
+        assert gt.gate_feature(feat="area_um", data=55)
+        assert not gt.gate_feature(feat="area_um", data=48)
+
+
+def test_gate_mask():
+    path = retrieve_data(
+        "fmt-hdf5_cytoshot_full-features_legacy_allev_2023.zip")
+
+    with HDF5Data(path) as hd:
+        gt = Gate(data=hd, online_gates=False, size_thresh_mask=10)
+
+        mask1 = np.zeros((20, 20), dtype=bool)
+        mask1[2:4, 2:4] = True
+        assert not gt.gate_mask(mask1)
+        assert not gt.gate_mask(mask1, mask_sum=np.sum(mask1))
+
+        mask2 = np.zeros((20, 20), dtype=bool)
+        mask2[2:10, 2:10] = True
+        assert gt.gate_mask(mask2)
+        assert gt.gate_mask(mask2, mask_sum=np.sum(mask2))
 
 
 def test_get_ppkw_from_ppid():
