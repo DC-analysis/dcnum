1505e04adde571bc141e57a2ec1901456e5f1923
Paul Müller <dev@craban.de>
2024-05-05 15:41:10 +0200

    increase coverage

--- tests/test_segm_no_mask_proc.py
+++ tests/test_segm_no_mask_proc.py
@@ -1,3 +1,4 @@
+# flake8: noqa: F821
 import pathlib
 
 import pytest
@@ -10,11 +11,27 @@ SEGM_KEYS = sorted(SEGM_METH.keys())
 
 
 def test_ppid_nomask_segmenter():
-    class SegmentNoMask(segm.segm_thresh.SegmentThresh):
+    class SegmentNoMask(segm.CPUSegmenter):
         mask_postprocessing = False
+
+        def __init__(self, thresh=-6, *args, **kwargs):
+            """Threshold for testing without mask"""
+            super(SegmentNoMask, self).__init__(thresh=thresh, *args, **kwargs)
+
+        @staticmethod
+        def segment_approach(image, *,
+                             thresh: float = -6):
+            return image < thresh
+
+    segm.get_available_segmenters.cache_clear()
 
     ppid1 = SegmentNoMask.get_ppid_from_ppkw({"thresh": -3})
     assert ppid1 == "nomask:t=-3"
+
+    ppkw1 = SegmentNoMask.get_ppkw_from_ppid("nomask:t=-5")
+    assert "kwargs_mask" not in ppkw1
+    assert len(ppkw1) == 1
+    assert ppkw1["thresh"] == -5
 
     with pytest.raises(ValueError,
                        match="does not support mask postprocessing"):
@@ -24,10 +41,10 @@ def test_ppid_nomask_segmenter():
 
     # cleanup
     del SegmentNoMask
+    segm.get_available_segmenters.cache_clear()
 
 
 def test_ppid_nomask_segmenter_control():
-
     with pytest.raises(KeyError,
                        match="must be either specified as keyword argument"):
         segm.segm_thresh.SegmentThresh.get_ppid_from_ppkw({"thresh": -3})
