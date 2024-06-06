a3a16692157c803b8607515b67a3ba1855424fd4
Paul Müller <dev@craban.de>
2024-06-06 23:05:57 +0200

    enh: introduce `Segmenter.validate_applicability` method

--- src/dcnum/segm/__init__.py
+++ src/dcnum/segm/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa: F401
-from .segmenter import Segmenter, get_available_segmenters
+from .segmenter import (
+    Segmenter, SegmenterNotApplicableError, get_available_segmenters
+)
 from .segmenter_mpo import MPOSegmenter
 from .segmenter_sto import STOSegmenter
 from .segmenter_manager_thread import SegmenterManagerThread
--- src/dcnum/segm/segm_torch/segm_torch_base.py
+++ src/dcnum/segm/segm_torch/segm_torch_base.py
@@ -1,8 +1,13 @@
+import functools
 import pathlib
+import re
+from typing import Dict
 
 from ...meta import paths
 
-from ..segmenter import Segmenter
+from ..segmenter import Segmenter, SegmenterNotApplicableError
+
+from .torch_model import load_model
 
 
 class TorchSegmenterBase(Segmenter):
@@ -31,3 +36,90 @@ class TorchSegmenterBase(Segmenter):
                 kwargs_new["model_file"] = mpath.name
         return super(TorchSegmenterBase, cls).get_ppid_from_ppkw(kwargs_new,
                                                                  kwargs_mask)
+
+    @classmethod
+    def validate_applicability(cls,
+                               segmenter_kwargs: Dict,
+                               meta: Dict = None,
+                               logs: Dict = None):
+        """Validate the applicability of this segmenter for a dataset
+
+        The applicability is defined by the metadata in the segmentation
+        model.
+
+        Parameters
+        ----------
+        segmenter_kwargs: dict
+            Keyword arguments for the segmenter
+        meta: dict
+            Dictionary of metadata from an :class:`HDF5Data` instance
+        logs: dict
+            Dictionary of logs from an :class:`HDF5Data` instance
+
+        Returns
+        -------
+        applicable: bool
+            True if the segmenter is applicable to the dataset
+
+        Raises
+        ------
+        SegmenterNotApplicable
+            If the segmenter is not applicable to the dataset
+        """
+        if "model_file" not in segmenter_kwargs:
+            raise ValueError("A `model_file` must be provided in the "
+                             "`segmenter_kwargs` to validate applicability")
+
+        model_file = segmenter_kwargs["model_file"]
+        _, model_meta = load_model(model_file, device="cpu")
+
+        reasons_list = []
+        validators = {
+            "meta": functools.partial(
+                cls._validate_applicability_item,
+                data_dict=meta,
+                reasons_list=reasons_list),
+            "logs": functools.partial(
+                cls._validate_applicability_item,
+                # convert logs to strings
+                data_dict={key: "\n".join(val) for key, val in logs.items()},
+                reasons_list=reasons_list)
+        }
+        for item in model_meta.get("validation", []):
+            it = item["type"]
+            if it in validators:
+                validators[it](item)
+            else:
+                reasons_list.append(
+                    f"invalid validation type {it} in {model_file}")
+
+        if reasons_list:
+            raise SegmenterNotApplicableError(segmenter_class=cls,
+                                              reasons_list=reasons_list)
+
+        return True
+
+    @staticmethod
+    def _validate_applicability_item(item, data_dict, reasons_list):
+        """Populate `reasons_list` with invalid entries
+
+        Example `data_dict`::
+
+            {"type": "meta",
+             "key": "setup:region",
+             "allow-missing-key": False,
+             "regexp": "^channel$",
+             "regexp-negate": False,
+             "reason": "only channel region supported",
+             }
+        """
+        key = item["key"]
+        if key in data_dict:
+            regexp = re.compile(item["regexp"])
+            matched = bool(regexp.match(data_dict[key]))
+            negate = item.get("regexp-negate", False)
+            valid = matched if not negate else not matched
+            if not valid:
+                reasons_list.append(item.get("reason", "unknown reason"))
+        elif not item.get("allow-missing-key", False):
+            reasons_list.append(f"Key {key} missing in {item['type']}")
