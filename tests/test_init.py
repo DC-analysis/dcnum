f9a440a2ba2f7e5394144a24a50241e309720f85
Pascal Proske <pascal@aviselabs.de>
2025-03-10 20:22:34 +0100

    write a comment
    write a comment for test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,4 +2,6 @@ import dcnum
 
 
 def test_init():
+    # Checks if the object `dcnum` has an attribute named `__version__`.
+    # If not, an AssertionError is raised.
     assert hasattr(dcnum, "__version__")
