e44511458a568a4b6b045057fa6f26f378bc291f
Paul Müller <dev@craban.de>
2025-11-27 11:46:30 +0100

    fix tests

--- tests/test_read_cache.py
+++ tests/test_read_cache.py
@@ -11,17 +11,14 @@ from helper_methods import retrieve_data
 
 def test_image_read_cache_min_size():
     h5path = retrieve_data("fmt-hdf5_cytoshot_full-features_2023.zip")
-    with h5py.File(h5path) as h5:
-        im = HDF5ImageCache(h5["events/image"])
-        assert im.chunk_size == 40, "because that is the total size"
+    with HDF5Data(h5path) as hd:
+        assert hd.image.chunk_size == 40, "because that is the total size"
 
 
 def test_image_read_cache_auto_min_size():
     h5path = retrieve_data("fmt-hdf5_cytoshot_full-features_2023.zip")
-    with h5py.File(h5path) as h5:
-        im = HDF5ImageCache(h5["events/image"],
-                            chunk_size=23)
-        assert im.chunk_size == 40, "because that is the minimum size"
+    with HDF5Data(h5path, image_chunk_size=23) as hd:
+        assert hd.image.chunk_size == 40, "because that is the minimum size"
 
 
 def test_image_read_cache_auto_max_size():
@@ -36,11 +33,9 @@ def test_image_read_cache_auto_max_size():
         del hw.h5["events/image"]
         hw.store_feature_chunk("image", images)
 
-    with h5py.File(path) as h5:
-        assert h5["events/image"].chunks == (32, 80, 400)
-        im = HDF5ImageCache(h5["events/image"],
-                            chunk_size=30)
-        assert im.chunk_size == 32, "chunking increased to minimum 32"
+    with HDF5Data(path, image_chunk_size=30) as hd:
+        assert hd.h5["events/image"].chunks == (32, 80, 400)
+        assert hd.image.chunk_size == 32, "chunking increased to minimum 32"
 
 
 def test_image_read_cache_auto_reduced():
@@ -55,11 +50,9 @@ def test_image_read_cache_auto_reduced():
         del hw.h5["events/image"]
         hw.store_feature_chunk("image", images)
 
-    with h5py.File(path) as h5:
-        assert h5["events/image"].chunks == (32, 80, 400)
-        im = HDF5ImageCache(h5["events/image"],
-                            chunk_size=81)
-        assert im.chunk_size == 64, "chunking reduced to maximum below 81"
+    with HDF5Data(path, image_chunk_size=81) as hd:
+        assert hd.h5["events/image"].chunks == (32, 80, 400)
+        assert hd.image.chunk_size == 64, "chunking reduced to maximum below 81"
 
 
 @pytest.mark.parametrize("chunk_size", (32, 64, 1000))
@@ -87,8 +80,6 @@ def test_image_read_cache_contiguous():
     with concatenated_hdf5_data(10 * [h5path], path_out=path):
         pass
 
-    with h5py.File(path) as h5:
-        assert h5["events/image"].chunks is None
-        im = HDF5ImageCache(h5["events/image"],
-                            chunk_size=81)
-        assert im.chunk_size == 81, "chunking as requested, contiguous arrays"
+    with HDF5Data(path, image_chunk_size=81) as hd:
+        assert hd.h5["events/image"].chunks is None
+        assert hd.image.chunk_size == 81, "chunking as requested, contiguous arrays"
